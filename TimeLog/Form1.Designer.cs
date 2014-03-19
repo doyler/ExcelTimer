16609de7cd13d862b61cfc98928a47f77b7c706f
doyler <dev@doyler.net>
2014-03-19 15:50:37 -0400

    Initial commit, very basic functionality

---
+++ TimeLog/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace TimeLog
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.theTime = new System.Windows.Forms.Label();
+            this.myTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(86, 132);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(86, 161);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(86, 190);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // theTime
+            // 
+            this.theTime.AutoSize = true;
+            this.theTime.Location = new System.Drawing.Point(83, 101);
+            this.theTime.Name = "theTime";
+            this.theTime.Size = new System.Drawing.Size(49, 13);
+            this.theTime.TabIndex = 3;
+            this.theTime.Text = "00:00:00";
+            // 
+            // myTimer
+            // 
+            this.myTimer.Interval = 10;
+            this.myTimer.Tick += new System.EventHandler(this.myTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.theTime);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label theTime;
+        private System.Windows.Forms.Timer myTimer;
+    }
+}
+
---
+++ TimeLog/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/* To work eith EPPlus library */
+using OfficeOpenXml;
+using OfficeOpenXml.Drawing;
+
+/* For I/O purpose */
+using System.IO;
+
+/* For Diagnostics */
+using System.Diagnostics;
+
+namespace TimeLog
+{
+    public partial class Form1 : Form
+    {
+        private int timeCounter = 0;
+        private DateTime startTime = new DateTime();
+        private DateTime endTime = new DateTime();
+
+        string path = Directory.GetCurrentDirectory();
+
+        int startRow = 1;
+        int maxRow = 1048576; //max row in excel
+        int maxCol = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            myTimer.Start();
+            startTime = DateTime.Now;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            myTimer.Stop();
+            endTime = DateTime.Now;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Get the file we are going to process
+                var existingFile = new FileInfo("TimeLog.xlsx");
+
+                // Open and read the XlSX file.
+                using (var package = new ExcelPackage(existingFile))
+                {
+                    // Get the work book in the file
+                    ExcelWorkbook workBook = package.Workbook;
+                    if (workBook != null)
+                    {
+                        if (workBook.Worksheets.Count > 0)
+                        {
+                            // Get the first worksheet
+                            ExcelWorksheet currentWorksheet = workBook.Worksheets.First();
+
+                            // read some data
+                            object col1Header = currentWorksheet.Cells[startRow, 1].Value;
+                            object col2Header = currentWorksheet.Cells[startRow, 2].Value;
+
+                            if (((col1Header != null) && (col1Header.ToString() == "Start Time")) && ((col2Header != null) && (col2Header.ToString() == "End Time")))
+                            {
+                                int endRow = currentWorksheet.Dimension.End.Row + 1;
+
+                                for (int rowNumber = startRow + 1; rowNumber <= endRow; rowNumber++)
+                                // read each row from the start of the data (start row + 1 header row) to the end of the spreadsheet.
+                                {
+                                    object col1Value = currentWorksheet.Cells[rowNumber, 1].Value;
+                                    object col2Value = currentWorksheet.Cells[rowNumber, 2].Value;
+
+                                    if ((col1Value == null) && (col2Value == null))
+                                    {
+                                        currentWorksheet.Cells[rowNumber, 1].Value = startTime.ToString();
+                                        currentWorksheet.Cells[rowNumber, 2].Value = endTime.ToString();
+                                        currentWorksheet.Cells[rowNumber, 3].Value = 0.1 * Math.Ceiling(10 * ((timeCounter / (60.0 * 60.0)) / 24.0));
+                                        package.Save();
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Example data incorrectly formatted.");
+                            }
+                        }
+                    }
+                    workBook = null;
+                }
+            }
+            catch (Exception ex)
+            {
+                showError(ex.ToString());
+                showError(ex.Message);
+            }
+
+            //showError(myStopwatch.Elapsed.ToString());
+        }
+
+        private void showError(string theError)
+        {
+            MessageBox.Show(theError, "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1);
+        }
+
+        private void myTimer_Tick(object sender, EventArgs e)
+        {
+            theTime.Text = ((timeCounter / (60 * 60)) % 24).ToString().PadLeft(2, '0') + ":" + ((timeCounter / 60) % 60).ToString().PadLeft(2, '0') + ":" + (timeCounter % 60).ToString().PadLeft(2, '0');
+            timeCounter++;
+        }
+    }
+}
